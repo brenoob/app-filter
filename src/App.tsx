36ece2b7ff1b84aa4b7ef4c47e3d51fb0070d709
brenoob <breno_1993@outlook.com>
2024-02-21 22:51:54 -0300

    feat: base UI completa sem nenhuma funcionalidade so interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-// import { useState } from 'react'
-import { Plus } from "lucide-react"
+import { FileDown, MoreHorizontal, Plus, Search } from "lucide-react"
 import { Header } from "./components/header"
 import { Tabs } from "./components/tabs"
+import { Button } from "./components/ui/button"
+import { Control, Input } from "./components/ui/input"
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "./components/ui/table"
+import { Pagination } from "./components/pagination"
 
 function App() {
-  // const [count, setCount] = useState(0)
 
   return (
     <div className="py-10 space-y-8">
@@ -12,17 +14,65 @@ function App() {
         <Header />
         <Tabs />
       </div>
-      <div>
-        <main className="max-w-6xl mx-auto space-y-5">
-          <div className="flex items-center gap-3">
+      <main className="max-w-6xl mx-auto space-y-5">
+        <div className="flex items-center gap-3">
           <h1 className="text-xl font-bold">Tags</h1>
-          <button className="inline-flex items-center gap-1.5 text-xs bg-teal-300 text-teal-950 font-medium rounded-full px-1.5 py-1">
+          <Button variant="primary">
             <Plus className="size-3" />
             Create new
-          </button>
-          </div>
-        </main>
-      </div>
+          </Button>
+        </div>
+
+        <div className="flex items-center justify-between">
+          <Input variant="filter">
+            <Search className="size-3" />
+            <Control placeholder="Search Tags" />
+          </Input>
+
+          <Button>
+            <FileDown className="size-3" />
+            Export
+          </Button>
+        </div>
+
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead></TableHead>
+              <TableHead>tag</TableHead>
+              <TableHead>Amount of videos</TableHead>
+              <TableHead></TableHead>
+            </TableRow>
+          </TableHeader>
+
+          <TableBody>
+            {Array.from({ length: 10 }).map((value, index) => {
+              return (
+                <TableRow key={index}>
+                  <TableCell></TableCell>
+                  <TableCell>
+                    <div className="flex flex-col gap-0.5">
+                      <span className="font-medium">React</span>
+                      <span className="text-xs text-zinc-500">9d0b6e89-a4e5-4bb2-8763-4a5077e6ca44</span>
+                    </div>
+                  </TableCell>
+                  <TableCell className="text-zinc-300">
+                    19 video(s)
+                  </TableCell>
+                  <TableCell className="text-right">
+                    <Button size="icon">
+                      <MoreHorizontal className="size-4" />
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              )
+            })}
+          </TableBody>
+        </Table>
+
+<Pagination />
+
+      </main>
     </div>
   )
 }
